88e3eb87c1c9e603a4d86f1a11d818e2e65e6888
OlgaShopieva <shopieva94@gmail.com>
2022-11-09 22:38:20 +0300

    added second array

--- ArrayUpto3/Program.cs
+++ ArrayUpto3/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] firstArray = new string[6] { "12345", "23", "GeekBrains", "computer science", "world", "-45" };
+string[] secondArray = new string[firstArray.Length];
+
+FillSecondArray(firstArray, econdArray);
+PrintArray(secondArray);
+
+void FillSecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
